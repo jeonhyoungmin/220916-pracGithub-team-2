74981568f1f7605a4e916f7f1f5c78f7274afc62
jeonhyoungmin <jhmin7509@gmail.com>
2022-09-16 15:14:47 +0900

    기본 세팅

---
+++ main.js
@@ -0,0 +1,12 @@
+// * main.js
+
+// * 루트 js 파일
+
+const root = document.getElementById('root');
+console.log(root)
+root.style.width = "100vw"
+root.style.height = "100vh"
+root.style.display = "flex"
+root.style.justifyContent = "center"
+root.style.alignItems = "center"
+root.style.backgroundColor = "cadetblue"

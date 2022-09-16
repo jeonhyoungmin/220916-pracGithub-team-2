855c127ba60ebb1d3adc255ae5e14bbfaac10ffa
jeonhyoungmin <jhmin7509@gmail.com>
2022-09-16 16:30:44 +0900

    모듈 사만팔천

--- module-1.js
+++
@@ -1,9 +0,0 @@
-function createTag(tagName, props, parent){
-  let childElement = document.createElement(tagName)
-  for(let cssValue in props){
-    childElement.style[cssValue] = props[cssValue]
-  }
-  return parent.appendChild(childElement)
-}
-
-export default createTag
--- module-2.js
+++
@@ -1,8 +0,0 @@
-
-function DTG(tagName) {
-  tagName.addEventListener('click', function () {
-    console.log('두더지')
-  })
-}
-
-export default DTG
